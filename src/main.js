099b408afe8b21cc9f1d343b1476b3f669c0ebf9
Serhii01010101 <jobjob2109@gmail.com>
2024-09-09 17:59:27 +0300

    +

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './js/about-me';
+import './js/benefits.js';
+import './js/covers.js';
+import './js/faq.js';
+import './js/header.js';
+import './js/projects.js';
+import './js/reviews.js';
+import './js/swagger-api.js';
+import './js/work-together.js';
+import './js/scroll-to-top.js';
+import './js/animations.js';
+//
